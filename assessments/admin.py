c80ea1704787dccab2cd5543a16861be58753403
Wesley Lawson <wesleylawson13@gmail.com>
2025-03-30 21:12:50 -0400

    registering 'Assessment' model

--- assessments/admin.py
+++ assessments/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Course, Team
+from .models import Course, Team, Assessment
 
 # Register your models here.
 admin.site.register(Course)
 admin.site.register(Team)
+admin.site.register(Assessment)
