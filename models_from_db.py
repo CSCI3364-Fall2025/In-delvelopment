2faee8c01b910f528132df64d50232a39471f3e4
noobyanjunhao <yanaw@bc.edu>
2025-04-15 00:41:53 -0400

    updated requirements and checked database models

---
+++ models_from_db.py
@@ -0,0 +1,405 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
+
+class AccountEmailaddress(models.Model):
+    verified = models.BooleanField()
+    primary = models.BooleanField()
+    user = models.ForeignKey('AuthUser', models.DO_NOTHING)
+    email = models.CharField(unique=True, max_length=254)
+
+    class Meta:
+        managed = False
+        db_table = 'account_emailaddress'
+        unique_together = (('user', 'primary'), ('user', 'email'),)
+
+
+class AccountEmailconfirmation(models.Model):
+    created = models.DateTimeField()
+    sent = models.DateTimeField(blank=True, null=True)
+    key = models.CharField(unique=True, max_length=64)
+    email_address = models.ForeignKey(AccountEmailaddress, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'account_emailconfirmation'
+
+
+class AssessmentsAssessment(models.Model):
+    title = models.CharField(max_length=200)
+    due_date = models.DateTimeField(blank=True, null=True)
+    closed_date = models.DateTimeField(blank=True, null=True)
+    open_date = models.DateTimeField(blank=True, null=True)
+    self_assessment_required = models.BooleanField()
+    published_date = models.DateTimeField(blank=True, null=True)
+    results_published = models.BooleanField()
+    course = models.ForeignKey('AssessmentsCourse', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'assessments_assessment'
+
+
+class AssessmentsAssessmentscore(models.Model):
+    score = models.FloatField()
+    assessment = models.ForeignKey(AssessmentsAssessment, models.DO_NOTHING)
+    student = models.ForeignKey('AuthUser', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'assessments_assessmentscore'
+
+
+class AssessmentsAssessmentsubmission(models.Model):
+    student = models.CharField(max_length=255)
+    contribution = models.IntegerField()
+    teamwork = models.IntegerField()
+    communication = models.IntegerField()
+    feedback = models.TextField()
+    assessment = models.ForeignKey(AssessmentsAssessment, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'assessments_assessmentsubmission'
+
+
+class AssessmentsCourse(models.Model):
+    name = models.CharField(max_length=100)
+    course_code = models.CharField(max_length=10)
+    description = models.TextField()
+    is_active = models.BooleanField()
+    created_by = models.ForeignKey('AuthUser', models.DO_NOTHING)
+    semester = models.CharField(max_length=6)
+    year = models.CharField(max_length=4)
+
+    class Meta:
+        managed = False
+        db_table = 'assessments_course'
+
+
+class AssessmentsCourseStudents(models.Model):
+    course = models.ForeignKey(AssessmentsCourse, models.DO_NOTHING)
+    user = models.ForeignKey('AuthUser', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'assessments_course_students'
+        unique_together = (('course', 'user'),)
+
+
+class AssessmentsPeerassessment(models.Model):
+    title = models.CharField(max_length=255)
+    publication_date = models.DateTimeField()
+    closing_date = models.DateTimeField()
+    publication_email_sent = models.BooleanField()
+
+    class Meta:
+        managed = False
+        db_table = 'assessments_peerassessment'
+
+
+class AssessmentsSubmission(models.Model):
+    submitted_at = models.DateTimeField()
+    assessment = models.ForeignKey(AssessmentsPeerassessment, models.DO_NOTHING)
+    student = models.ForeignKey('AuthUser', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'assessments_submission'
+
+
+class AssessmentsTeam(models.Model):
+    name = models.CharField(max_length=50)
+    is_active = models.BooleanField()
+    course = models.ForeignKey(AssessmentsCourse, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'assessments_team'
+
+
+class AssessmentsTeamMembers(models.Model):
+    team = models.ForeignKey(AssessmentsTeam, models.DO_NOTHING)
+    user = models.ForeignKey('AuthUser', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'assessments_team_members'
+        unique_together = (('team', 'user'),)
+
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+    first_name = models.CharField(max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class AuthenticationAssessmentprogress(models.Model):
+    last_updated = models.DateTimeField()
+    assessment = models.ForeignKey(AssessmentsAssessment, models.DO_NOTHING)
+    student = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    progress_notes = models.JSONField()
+
+    class Meta:
+        managed = False
+        db_table = 'authentication_assessmentprogress'
+
+
+class AuthenticationUserprofile(models.Model):
+    role = models.CharField(max_length=10)
+    user = models.OneToOneField(AuthUser, models.DO_NOTHING)
+    progress_data = models.JSONField()
+    preferred_name = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'authentication_userprofile'
+
+
+class DjangoAdminLog(models.Model):
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    action_time = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoCeleryBeatClockedschedule(models.Model):
+    clocked_time = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_celery_beat_clockedschedule'
+
+
+class DjangoCeleryBeatCrontabschedule(models.Model):
+    minute = models.CharField(max_length=240)
+    hour = models.CharField(max_length=96)
+    day_of_week = models.CharField(max_length=64)
+    day_of_month = models.CharField(max_length=124)
+    month_of_year = models.CharField(max_length=64)
+    timezone = models.CharField(max_length=63)
+
+    class Meta:
+        managed = False
+        db_table = 'django_celery_beat_crontabschedule'
+
+
+class DjangoCeleryBeatIntervalschedule(models.Model):
+    every = models.IntegerField()
+    period = models.CharField(max_length=24)
+
+    class Meta:
+        managed = False
+        db_table = 'django_celery_beat_intervalschedule'
+
+
+class DjangoCeleryBeatPeriodictask(models.Model):
+    name = models.CharField(unique=True, max_length=200)
+    task = models.CharField(max_length=200)
+    args = models.TextField()
+    kwargs = models.TextField()
+    queue = models.CharField(max_length=200, blank=True, null=True)
+    exchange = models.CharField(max_length=200, blank=True, null=True)
+    routing_key = models.CharField(max_length=200, blank=True, null=True)
+    expires = models.DateTimeField(blank=True, null=True)
+    enabled = models.BooleanField()
+    last_run_at = models.DateTimeField(blank=True, null=True)
+    total_run_count = models.PositiveIntegerField()
+    date_changed = models.DateTimeField()
+    description = models.TextField()
+    crontab = models.ForeignKey(DjangoCeleryBeatCrontabschedule, models.DO_NOTHING, blank=True, null=True)
+    interval = models.ForeignKey(DjangoCeleryBeatIntervalschedule, models.DO_NOTHING, blank=True, null=True)
+    solar = models.ForeignKey('DjangoCeleryBeatSolarschedule', models.DO_NOTHING, blank=True, null=True)
+    one_off = models.BooleanField()
+    start_time = models.DateTimeField(blank=True, null=True)
+    priority = models.PositiveIntegerField(blank=True, null=True)
+    headers = models.TextField()
+    clocked = models.ForeignKey(DjangoCeleryBeatClockedschedule, models.DO_NOTHING, blank=True, null=True)
+    expire_seconds = models.PositiveIntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'django_celery_beat_periodictask'
+
+
+class DjangoCeleryBeatPeriodictasks(models.Model):
+    ident = models.AutoField(primary_key=True)
+    last_update = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_celery_beat_periodictasks'
+
+
+class DjangoCeleryBeatSolarschedule(models.Model):
+    event = models.CharField(max_length=24)
+    latitude = models.DecimalField(max_digits=10, decimal_places=5)  # max_digits and decimal_places have been guessed, as this database handles decimal fields as float
+    longitude = models.DecimalField(max_digits=10, decimal_places=5)  # max_digits and decimal_places have been guessed, as this database handles decimal fields as float
+
+    class Meta:
+        managed = False
+        db_table = 'django_celery_beat_solarschedule'
+        unique_together = (('event', 'latitude', 'longitude'),)
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
+class DjangoSite(models.Model):
+    name = models.CharField(max_length=50)
+    domain = models.CharField(unique=True, max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_site'
+
+
+class SocialaccountSocialaccount(models.Model):
+    provider = models.CharField(max_length=200)
+    uid = models.CharField(max_length=191)
+    last_login = models.DateTimeField()
+    date_joined = models.DateTimeField()
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    extra_data = models.JSONField()
+
+    class Meta:
+        managed = False
+        db_table = 'socialaccount_socialaccount'
+        unique_together = (('provider', 'uid'),)
+
+
+class SocialaccountSocialapp(models.Model):
+    provider = models.CharField(max_length=30)
+    name = models.CharField(max_length=40)
+    client_id = models.CharField(max_length=191)
+    secret = models.CharField(max_length=191)
+    key = models.CharField(max_length=191)
+    provider_id = models.CharField(max_length=200)
+    settings = models.JSONField()
+
+    class Meta:
+        managed = False
+        db_table = 'socialaccount_socialapp'
+
+
+class SocialaccountSocialappSites(models.Model):
+    socialapp = models.ForeignKey(SocialaccountSocialapp, models.DO_NOTHING)
+    site = models.ForeignKey(DjangoSite, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'socialaccount_socialapp_sites'
+        unique_together = (('socialapp', 'site'),)
+
+
+class SocialaccountSocialtoken(models.Model):
+    token = models.TextField()
+    token_secret = models.TextField()
+    expires_at = models.DateTimeField(blank=True, null=True)
+    account = models.ForeignKey(SocialaccountSocialaccount, models.DO_NOTHING)
+    app = models.ForeignKey(SocialaccountSocialapp, models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'socialaccount_socialtoken'
+        unique_together = (('app', 'account'),)
