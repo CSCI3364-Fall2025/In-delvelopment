743c283f8d32aaf2316b9faf1a7fb848575a78f4
indelgarcia <indelg7@gmail.com>
2025-11-10 19:35:46 -0500

    fixing static files, failed test

--- PeerAssess/settings.py
+++ PeerAssess/settings.py
@@ -122,6 +122,13 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+from pathlib import Path
+STATIC_DIR = BASE_DIR / "static"
+if STATIC_DIR.exists():
+    STATICFILES_DIRS = [STATIC_DIR]
+else:
+    STATICFILES_DIRS = []
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
