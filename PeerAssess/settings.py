5daae427043a4e8f3f31a474fe813d5b114d4126
indelgarcia <indelg7@gmail.com>
2025-11-10 20:40:43 -0500

    lets see if this works...

--- PeerAssess/settings.py
+++ PeerAssess/settings.py
@@ -120,14 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-
-from pathlib import Path
-STATIC_DIR = BASE_DIR / "static"
-if STATIC_DIR.exists():
-    STATICFILES_DIRS = [STATIC_DIR]
-else:
-    STATICFILES_DIRS = []
+STATICFILES_DIRS = [BASE_DIR / "static"] if (BASE_DIR / "static").exists() else []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
